bcc8f4f5710df98bd16f4933f708a94b3b2c8f5c
doukang <742976405@qq.com>
2020-04-13 17:47:35 +0800

    测试云开发

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,21 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+//云开发数据库
+const DB = wx.cloud.database().collection("list")
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  addData() {
+    DB.add({
+      data:{
+        name:'岳凯01',
+        age:32
+      },
+      success(res){
+        console.log("添加成功", res)
+      },
+      fail(res) {
+        console.log("添加失败", res)
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })
