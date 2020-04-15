a143bff934c1eb04dc47aade61dfd9bfb5419187
dukang <742976405@qq.com>
2020-04-16 07:53:16 +0800

    引入colorUI组件库
    1.引入colorUI组件库
    2.取消小程序默认导航栏
    3.删除测试云开发代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,18 +4,5 @@ const app = getApp()
 //云开发数据库
 const DB = wx.cloud.database().collection("list")
 Page({
-  addData() {
-    DB.add({
-      data:{
-        name:'岳凯01',
-        age:32
-      },
-      success(res){
-        console.log("添加成功", res)
-      },
-      fail(res) {
-        console.log("添加失败", res)
-      }
-    })
-  }
+  
 })
